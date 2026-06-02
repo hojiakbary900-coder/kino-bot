fe081e497f22b8a72df9faa3d5fe7da3f235850e
hojiakbary900-coder <hojiakbary900@gmail.com>
2026-06-02 20:13:55 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "8894990730:AAHTsBhrI8sk8-zBBC_pfC7yIvyTfjGy5Mg"
+
+ADMIN_ID = 6401247171
+
+CHANNELS = [
+    "@VIP_drama_uz01",
+    "@Dramalar_olami_uzz",
+    "@daxshat_kinolar_uzzz"
+]
+
+CHANNEL_POST = "@daxshat_kinolar_uzzz"
